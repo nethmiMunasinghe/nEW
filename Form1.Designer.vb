4e739f307dc1c5dd7d225fef2fcb7a6a8fa68685
Nethmi <Nethmi@DESKTOP-E32442F>
2025-10-20 19:55:19 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,331 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.txtIndex = New System.Windows.Forms.TextBox()
+        Me.btnAverage = New System.Windows.Forms.Button()
+        Me.btnGrade = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtSub1 = New System.Windows.Forms.TextBox()
+        Me.txtSub2 = New System.Windows.Forms.TextBox()
+        Me.lblIndex = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblSub1 = New System.Windows.Forms.Label()
+        Me.lblSub2 = New System.Windows.Forms.Label()
+        Me.lblGrade = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblSub3 = New System.Windows.Forms.Label()
+        Me.txtGrade = New System.Windows.Forms.TextBox()
+        Me.txtAverage = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtSub3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnTotal
+        '
+        Me.btnTotal.BackColor = System.Drawing.Color.Fuchsia
+        Me.btnTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTotal.Location = New System.Drawing.Point(558, 280)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(146, 76)
+        Me.btnTotal.TabIndex = 0
+        Me.btnTotal.Text = "Find Total"
+        Me.btnTotal.UseVisualStyleBackColor = False
+        '
+        'txtIndex
+        '
+        Me.txtIndex.BackColor = System.Drawing.Color.Lime
+        Me.txtIndex.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtIndex.Location = New System.Drawing.Point(180, 96)
+        Me.txtIndex.Name = "txtIndex"
+        Me.txtIndex.Size = New System.Drawing.Size(285, 27)
+        Me.txtIndex.TabIndex = 1
+        '
+        'btnAverage
+        '
+        Me.btnAverage.BackColor = System.Drawing.Color.Fuchsia
+        Me.btnAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAverage.Location = New System.Drawing.Point(558, 401)
+        Me.btnAverage.Name = "btnAverage"
+        Me.btnAverage.Size = New System.Drawing.Size(146, 76)
+        Me.btnAverage.TabIndex = 2
+        Me.btnAverage.Text = "Find Average"
+        Me.btnAverage.UseVisualStyleBackColor = False
+        '
+        'btnGrade
+        '
+        Me.btnGrade.BackColor = System.Drawing.Color.Fuchsia
+        Me.btnGrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGrade.Location = New System.Drawing.Point(558, 538)
+        Me.btnGrade.Name = "btnGrade"
+        Me.btnGrade.Size = New System.Drawing.Size(146, 76)
+        Me.btnGrade.TabIndex = 3
+        Me.btnGrade.Text = "Find Grade"
+        Me.btnGrade.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(56, 652)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(146, 37)
+        Me.btnReset.TabIndex = 4
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(263, 652)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(146, 37)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'txtName
+        '
+        Me.txtName.BackColor = System.Drawing.Color.Lime
+        Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(180, 139)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(285, 27)
+        Me.txtName.TabIndex = 6
+        '
+        'txtSub1
+        '
+        Me.txtSub1.BackColor = System.Drawing.Color.Lime
+        Me.txtSub1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSub1.Location = New System.Drawing.Point(180, 183)
+        Me.txtSub1.Name = "txtSub1"
+        Me.txtSub1.Size = New System.Drawing.Size(285, 27)
+        Me.txtSub1.TabIndex = 7
+        '
+        'txtSub2
+        '
+        Me.txtSub2.BackColor = System.Drawing.Color.Lime
+        Me.txtSub2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSub2.Location = New System.Drawing.Point(180, 237)
+        Me.txtSub2.Name = "txtSub2"
+        Me.txtSub2.Size = New System.Drawing.Size(285, 27)
+        Me.txtSub2.TabIndex = 8
+        '
+        'lblIndex
+        '
+        Me.lblIndex.AutoSize = True
+        Me.lblIndex.BackColor = System.Drawing.Color.Red
+        Me.lblIndex.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblIndex.Location = New System.Drawing.Point(64, 102)
+        Me.lblIndex.Name = "lblIndex"
+        Me.lblIndex.Size = New System.Drawing.Size(85, 20)
+        Me.lblIndex.TabIndex = 9
+        Me.lblIndex.Text = "Index no:"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.Red
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(64, 142)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(63, 20)
+        Me.lblName.TabIndex = 10
+        Me.lblName.Text = "Name:"
+        '
+        'lblSub1
+        '
+        Me.lblSub1.AutoSize = True
+        Me.lblSub1.BackColor = System.Drawing.Color.Red
+        Me.lblSub1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSub1.Location = New System.Drawing.Point(64, 189)
+        Me.lblSub1.Name = "lblSub1"
+        Me.lblSub1.Size = New System.Drawing.Size(94, 20)
+        Me.lblSub1.TabIndex = 11
+        Me.lblSub1.Text = "Subject 1:"
+        '
+        'lblSub2
+        '
+        Me.lblSub2.AutoSize = True
+        Me.lblSub2.BackColor = System.Drawing.Color.Red
+        Me.lblSub2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSub2.Location = New System.Drawing.Point(64, 243)
+        Me.lblSub2.Name = "lblSub2"
+        Me.lblSub2.Size = New System.Drawing.Size(94, 20)
+        Me.lblSub2.TabIndex = 12
+        Me.lblSub2.Text = "Subject 2:"
+        '
+        'lblGrade
+        '
+        Me.lblGrade.AutoSize = True
+        Me.lblGrade.BackColor = System.Drawing.Color.Red
+        Me.lblGrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrade.Location = New System.Drawing.Point(64, 441)
+        Me.lblGrade.Name = "lblGrade"
+        Me.lblGrade.Size = New System.Drawing.Size(60, 20)
+        Me.lblGrade.TabIndex = 20
+        Me.lblGrade.Text = "Grade"
+        '
+        'lblAverage
+        '
+        Me.lblAverage.AutoSize = True
+        Me.lblAverage.BackColor = System.Drawing.Color.Red
+        Me.lblAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAverage.Location = New System.Drawing.Point(64, 387)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(83, 20)
+        Me.lblAverage.TabIndex = 19
+        Me.lblAverage.Text = "Average:"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.BackColor = System.Drawing.Color.Red
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(64, 340)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(57, 20)
+        Me.lblTotal.TabIndex = 18
+        Me.lblTotal.Text = "Total:"
+        '
+        'lblSub3
+        '
+        Me.lblSub3.AutoSize = True
+        Me.lblSub3.BackColor = System.Drawing.Color.Red
+        Me.lblSub3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSub3.Location = New System.Drawing.Point(64, 300)
+        Me.lblSub3.Name = "lblSub3"
+        Me.lblSub3.Size = New System.Drawing.Size(94, 20)
+        Me.lblSub3.TabIndex = 17
+        Me.lblSub3.Text = "Subject 3:"
+        '
+        'txtGrade
+        '
+        Me.txtGrade.BackColor = System.Drawing.Color.Lime
+        Me.txtGrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtGrade.Location = New System.Drawing.Point(180, 435)
+        Me.txtGrade.Name = "txtGrade"
+        Me.txtGrade.Size = New System.Drawing.Size(285, 27)
+        Me.txtGrade.TabIndex = 16
+        '
+        'txtAverage
+        '
+        Me.txtAverage.BackColor = System.Drawing.Color.Lime
+        Me.txtAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAverage.Location = New System.Drawing.Point(180, 381)
+        Me.txtAverage.Name = "txtAverage"
+        Me.txtAverage.Size = New System.Drawing.Size(285, 27)
+        Me.txtAverage.TabIndex = 15
+        '
+        'txtTotal
+        '
+        Me.txtTotal.BackColor = System.Drawing.Color.Lime
+        Me.txtTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTotal.Location = New System.Drawing.Point(180, 337)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(285, 27)
+        Me.txtTotal.TabIndex = 14
+        '
+        'txtSub3
+        '
+        Me.txtSub3.BackColor = System.Drawing.Color.Lime
+        Me.txtSub3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSub3.Location = New System.Drawing.Point(180, 294)
+        Me.txtSub3.Name = "txtSub3"
+        Me.txtSub3.Size = New System.Drawing.Size(285, 27)
+        Me.txtSub3.TabIndex = 13
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(546, 149)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(168, 56)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "Index Number And Name"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Blue
+        Me.ClientSize = New System.Drawing.Size(765, 748)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblGrade)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblSub3)
+        Me.Controls.Add(Me.txtGrade)
+        Me.Controls.Add(Me.txtAverage)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtSub3)
+        Me.Controls.Add(Me.lblSub2)
+        Me.Controls.Add(Me.lblSub1)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblIndex)
+        Me.Controls.Add(Me.txtSub2)
+        Me.Controls.Add(Me.txtSub1)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnGrade)
+        Me.Controls.Add(Me.btnAverage)
+        Me.Controls.Add(Me.txtIndex)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Name = "Form1"
+        Me.Text = "Find Total,Average ,Grade"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnTotal As Button
+    Friend WithEvents txtIndex As TextBox
+    Friend WithEvents btnAverage As Button
+    Friend WithEvents btnGrade As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtSub1 As TextBox
+    Friend WithEvents txtSub2 As TextBox
+    Friend WithEvents lblIndex As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblSub1 As Label
+    Friend WithEvents lblSub2 As Label
+    Friend WithEvents lblGrade As Label
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblSub3 As Label
+    Friend WithEvents txtGrade As TextBox
+    Friend WithEvents txtAverage As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtSub3 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+    Dim Sub1, Sub2, Sub3, Total, Average As Double
+    Dim Grade As String
+
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        Sub1 = Convert.ToDouble(txtSub1.Text)
+        Sub2 = Convert.ToDouble(txtSub2.Text)
+        Sub3 = Convert.ToDouble(txtSub3.Text)
+
+        ' Calculate total and average
+        Total = Sub1 + Sub2 + Sub3
+        txtTotal.Text = Total.ToString()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim IndexNo As String
+        Dim Name As String
+
+
+
+        IndexNo = InputBox("Enter Index Number")
+        txtIndex.Text = IndexNo
+        Name = InputBox("Enter Your Name")
+        txtName.Text = Name
+
+
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        txtIndex.Text = ""
+        txtName.Text = ""
+        txtSub1.Text = ""
+        txtSub2.Text = ""
+        txtSub3.Text = ""
+        txtTotal.Text = ""
+        txtAverage.Text = ""
+        txtGrade.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub btnAverage_Click(sender As Object, e As EventArgs) Handles btnAverage.Click
+        ' Convert text box values to numbers
+        Sub1 = Convert.ToDouble(txtSub1.Text)
+        Sub2 = Convert.ToDouble(txtSub2.Text)
+        Sub3 = Convert.ToDouble(txtSub3.Text)
+
+        ' Calculate total and average
+        Total = Sub1 + Sub2 + Sub3
+        Average = Total / 3
+
+        ' Update text boxes with calculated values
+        'txtTotal.Text = Total.ToString()
+        txtAverage.Text = Average.ToString()
+    End Sub
+
+    Private Sub btnGrade_Click(sender As Object, e As EventArgs) Handles btnGrade.Click
+        ' Ensure average is calculated first
+        Average = Convert.ToDouble(txtAverage.Text)
+
+        ' Determine grade based on average
+        If Average >= 75 And Average <= 100 Then
+            Grade = "A"
+        ElseIf Average >= 65 And Average < 75 Then
+            Grade = "B"
+        ElseIf Average >= 55 And Average < 65 Then
+            Grade = "C"
+        ElseIf Average >= 40 And Average < 55 Then
+            Grade = "S"
+        Else
+            Grade = "F"
+        End If
+
+        ' Update grade text box
+        txtGrade.Text = Grade
+    End Sub
+End Class
